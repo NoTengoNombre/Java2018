3dd8a898ddd2b695ca83dfd9a848ee12816dd997
MyDevelop <MyDevelop@192.168.1.56>
2018-12-07 01:35:04 +0100

    Interfaces 2

---
+++ MriadaxJava2/src/t2/basico/interfaces/ej/LetraNif.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t2.basico.interfaces.ej;
+
+/**
+ * @see p5
+ * @since 06-dic-2018
+ * @version 1.0
+ * @author Raul Vela Salas
+ */
+public class LetraNif {
+
+ @Override
+ public String toString() {
+  return super.toString(); //To change body of generated methods, choose Tools | Templates.
+ }
+
+}
